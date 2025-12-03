afc1c3750afebb05401b6823b8b3499fc6dcc241
Haiyuan Lin <haiyuan.lin@tuni.fi>
2025-12-04 00:18:49 +0200

    correct syntax

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-  res.send('<h1>Hello Watchtower!</h1>')
+  res.send('<h1>Hello World!</h1><h1>Hello Watchtower!</h1>')
 })
 
 const PORT = 8080
